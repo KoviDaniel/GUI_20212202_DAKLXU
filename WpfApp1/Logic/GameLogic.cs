b8fa0128cea19c37cf345c8ac667765c0868d780
Szilveszter Milán <milan.szilveszter@stud.uni-obuda.hu>
2022-04-19 10:42:11 +0200

    Minor changes.

--- WpfApp1/Logic/GameLogic.cs
+++ WpfApp1/Logic/GameLogic.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfApp1.Models;
 
 namespace WpfApp1.Logic
@@ -12,16 +13,16 @@ namespace WpfApp1.Logic
         Up, Down, Left, Right
     }
 
-    public class GameLogic
+    public class GameLogic : IGameModel
     {
-        private System.Windows.Size mapArea;
+        private Size mapArea;
         public event EventHandler Changed; //public event EventHandler GameOver;
         public Player Player { get; set; }
 
-        public void SetupSizes(System.Windows.Size mapArea)
+        public void SetupSizes(Size mapArea)
         {
             this.mapArea = mapArea;
-            Player = new Player(mapArea);
+            Player = new Player(mapArea.Width / 2, mapArea.Height / 2, 25, 25);
         }
 
         public GameLogic()
@@ -31,18 +32,27 @@ namespace WpfApp1.Logic
 
         public void Control(Controls control)
         {
-            switch (control) 
+            switch (control)
             {
                 case Controls.Up:
-                    Player.Move(mapArea);
+                    Player.Move(control);
                     break;
                 case Controls.Down:
+                    Player.Move(control);
                     break;
                 case Controls.Left:
+                    Player.Move(control);
                     break;
                 case Controls.Right:
+                    Player.Move(control);
                     break;
             }
+        }
+
+        public void TimeStep()
+        {
+            Rect playerRect = new Rect(Player.X, Player.Y, Player.PlayerWidth, Player.PlayerHeight);
+            Changed?.Invoke(this, null);
         }
     }
 }
---
+++ WpfApp1/Logic/IGameModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WpfApp1.Models;
+
+namespace WpfApp1.Logic
+{
+    public interface IGameModel
+    {
+        Player Player { get; set; }
+
+        event EventHandler Changed;
+    }
+}
