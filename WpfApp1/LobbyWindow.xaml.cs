4cc5749ff14f61a63a252a5608e41eb7e37dcab4
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-20 11:41:52 +0200

    implementing control  in xaml.cs

--- WpfApp1/LobbyWindow.xaml.cs
+++ WpfApp1/LobbyWindow.xaml.cs
@@ -11,7 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WpfApp1.Logic;
+using WpfApp1.Renderer;
 
 namespace MainMenu
 {
@@ -21,14 +23,52 @@ namespace MainMenu
     public partial class LobbyWindow : Window
     {
         private GameLogic gameLogic;
+        private Display display;
         public LobbyWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             gameLogic = new GameLogic();
+            display = new Display();
+            display.SetupModel(gameLogic);
+
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromMilliseconds(10);
+            dt.Tick += Dt_Tick;
+            dt.Start();
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            Control();
+            gameLogic.TimeStep();
+            this.InvalidateVisual();
+        }
+
+        private void Control() {
+            if (Keyboard.IsKeyDown(Key.W))
+            {
+                gameLogic.PlayerControl(Controls.Up);
+            }
+            if (Keyboard.IsKeyDown(Key.S))
+            {
+                gameLogic.PlayerControl(Controls.Down);
+
+            }
+            if (Keyboard.IsKeyDown(Key.A))
+            {
+                gameLogic.PlayerControl(Controls.Left);
+
+            }
+            if (Keyboard.IsKeyDown(Key.D))
+            {
+                gameLogic.PlayerControl(Controls.Right);
+            }
+
         }
     }
 }
