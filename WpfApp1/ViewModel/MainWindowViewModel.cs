9a0d45368daa75e9c9e7f3ab675aed755a15395e
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-20 12:10:52 +0200

    isInDesignMode, etc.. new command created

--- WpfApp1/ViewModel/MainWindowViewModel.cs
+++ WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace MainMenu.ViewModel
 {
     public class MainWindowViewModel : ObservableRecipient
     {
-
+        public ICommand LobbyOpener { get; set; }
+        static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
     }
 }
