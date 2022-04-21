35a36ead2f665358aef2e59731722567d8d77448
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-21 10:31:45 +0200

    the new following method is too CPU heavy

--- WpfApp1/Logic/GameLogic.cs
+++ WpfApp1/Logic/GameLogic.cs
@@ -48,7 +48,8 @@ namespace ShoresOfGold.Logic
             Rect playerRect = new Rect(Player.Center.X, Player.Center.Y, Player.Width, Player.Height);
             Rect zombieRect = new Rect(Zombie.Center.X, Zombie.Center.Y, Zombie.Width, Zombie.Height);
             // Zombie.FollowPlayer(Player, mapArea);
-            EnemyControl();
+            Zombie.NewFollowPlayer();
+            //EnemyControl();
             Changed?.Invoke(this, null);
         }
     }
