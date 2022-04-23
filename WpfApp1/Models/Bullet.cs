e6be21d1548881dedd4e8777377e3a70e24155b3
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-23 16:07:59 +0200

    bullet can move

--- WpfApp1/Models/Bullet.cs
+++ WpfApp1/Models/Bullet.cs
@@ -13,6 +13,8 @@ namespace ShoresOfGold.Models
         public Vector Speed { get;}
         public Vector Direction { get; }
 
+        private System.Drawing.Point center;
+
         private Vector DirectionVector(System.Drawing.Point start, System.Drawing.Point target) 
         {
             Vector v = new Vector();
@@ -26,8 +28,14 @@ namespace ShoresOfGold.Models
 
         public Bullet(System.Drawing.Point start, System.Drawing.Point target)
         {
+            this.center = start;
             Speed = new Vector(10, 10);
             Direction = DirectionVector(start, target);
+        }
+
+        public void Moving() 
+        {
+            this.center = new System.Drawing.Point(center.X + (int)(Direction.X * this.Speed.X), center.Y + (int)(Direction.Y * this.Speed.Y));
         }
     }
 }
