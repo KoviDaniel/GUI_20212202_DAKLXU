4e3d579a7997ee9d83936c254e99d227d9753d29
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-23 15:31:44 +0200

    Brute finished

--- WpfApp1/Models/Brute.cs
+++ WpfApp1/Models/Brute.cs
@@ -9,11 +9,33 @@ namespace ShoresOfGold.Models
 {
     public class Brute : Enemy
     {
-        
-
         public Brute(Size mapArea, Player player) : base(mapArea,player)
         {
+            Health = 200;
+            Stamina = 0; //TODO:? REMOVE
+            Power = 50; //Damage
 
+            Speed = new Vector(1.5, 1.5);
+            Width = 70;
+            Height = 70;
+
+            DetectionRange = 290;
+            AttackRange = 290;
+            StoppingRange = 10 + (player.Width + player.Height) / 2;
+
+            AttackIntensity = 150;
+        }
+        public override void Attack()
+        {
+            if (this.player != null)
+            {
+                cooldown++;
+                if (Distance <= AttackRange && cooldown >= AttackIntensity)
+                {
+                    this.player.GetDamage(this.Power);
+                    cooldown = 0;
+                }
+            }
         }
     }
 }
