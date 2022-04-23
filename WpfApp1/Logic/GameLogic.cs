7802a258dc012f4a28d47626e4e89d14a55adece
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-23 16:30:43 +0200

    EnemyControl is rewrited to handle a list of enemies

--- WpfApp1/Logic/GameLogic.cs
+++ WpfApp1/Logic/GameLogic.cs
@@ -63,14 +63,30 @@ namespace ShoresOfGold.Logic
         public void EnemyControl()
         {
             // Zombie.FollowPlayer(this.Player, this.mapArea);
-            Zombie.NewFollowPlayer();
-            Zombie.Attack();
+            /*Zombie.NewFollowPlayer();
+            Zombie.Attack();*/
+            foreach (var e in Enemies)
+            {
+                e.NewFollowPlayer();
+                if (e is Zombie) 
+                {
+                    (e as Zombie).Attack();
+                }
+                if (e is Brute) 
+                {
+                    (e as Brute).Attack();
+                }
+                if (e is Sniper)
+                {
+                    (e as Sniper).Attack();
+                }
+            }
         }
 
         public void TimeStep()
         {
             Rect playerRect = new Rect(Player.Center.X, Player.Center.Y, Player.Width, Player.Height);
-            Rect zombieRect = new Rect(Zombie.Center.X, Zombie.Center.Y, Zombie.Width, Zombie.Height);
+            //Rect zombieRect = new Rect(Zombie.Center.X, Zombie.Center.Y, Zombie.Width, Zombie.Height);
             // Zombie.FollowPlayer(Player, mapArea);
             
             EnemyControl();
