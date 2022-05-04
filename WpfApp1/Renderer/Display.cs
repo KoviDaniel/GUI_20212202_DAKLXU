afe6f97f285ca815124ad9e0a8f06f4b6fd6a9ba
Szilveszter Milán <milan.szilveszter@stud.uni-obuda.hu>
2022-05-04 15:01:19 +0200

    Player movement bounds in progress.

--- WpfApp1/Renderer/Display.cs
+++ WpfApp1/Renderer/Display.cs
@@ -34,16 +34,49 @@ namespace WpfApp1.Renderer
                 return new ImageBrush(new BitmapImage(new Uri("Images/pirate.png", UriKind.RelativeOrAbsolute)));
             }
         }
-
+        public Brush TopWallBrush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri("Images/back_decor.png", UriKind.RelativeOrAbsolute)));
+            }
+        }
+        public Brush BottomWallBrush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri("Images/front_decor.png", UriKind.RelativeOrAbsolute)));
+            }
+        }
+        
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
             if (mapArea.Width > 0 && mapArea.Height > 0 && model != null)
             {
-                drawingContext.DrawRectangle(PlayerBrush, null, new Rect(
-                    model.Player.Center.X, model.Player.Center.Y,
-                    model.Player.Width, model.Player.Height
-                    ));
+                drawingContext.DrawGeometry(
+                    TopWallBrush, new Pen(Brushes.Black, 1), model.TopWall.Area);
+                drawingContext.DrawGeometry(
+                    BottomWallBrush, new Pen(Brushes.Black, 1), model.BottomWall.Area);
+
+                var oldPosition = new Point(model.Player.Center.X, model.Player.Center.Y);
+
+                var upperBound = model.TopWall.Area.Bounds.Height;
+                var lowerBound = model.BottomWall.Area.Bounds.Y;
+
+                if (model.Player.Center.Y + model.Player.Height > upperBound && 
+                    model.Player.Center.Y + model.Player.Height < lowerBound && 
+                    model.Player.Center.X > 0 &&
+                    model.Player.Center.X + model.Player.Width < mapArea.Width) 
+                {
+                    drawingContext.DrawRectangle(PlayerBrush, new Pen(Brushes.Black, 1),
+                        new Rect(model.Player.Center.X, model.Player.Center.Y, model.Player.Width, model.Player.Height));
+                }
+                else
+                {
+                    drawingContext.DrawRectangle(PlayerBrush, new Pen(Brushes.Black, 1),
+                        new Rect(oldPosition.X, oldPosition.Y, model.Player.Width, model.Player.Height));
+                }
             }
         }
     }
