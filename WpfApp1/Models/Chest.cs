226031efecbeee42c072a598deed5140e70db22b
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-05-07 10:25:18 +0200

    Chest has an Open method which changes the player parameter depends its contained buff

--- WpfApp1/Models/Chest.cs
+++ WpfApp1/Models/Chest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ShoresOfGold.Models
 {
@@ -12,6 +13,34 @@ namespace ShoresOfGold.Models
     }
     public class Chest
     {
-        
+        Random r;
+        Buffs buff;
+        public Chest()
+        {
+            r = new Random();
+            buff = (Buffs)r.Next(0, 4);
+        }
+        public void Open(ref Player p) 
+        {
+            switch (buff)
+            {
+                case Buffs.HealthBuff:
+                    p.Health += 50;
+                    break;
+                case Buffs.MeleeDamageBuff:
+
+                    break;
+                case Buffs.RangeDamageBuff:
+                    break;
+                case Buffs.SpeedBuff:
+                    Vector v = new Vector();
+                    v.X = p.Speed.X + 0.15;
+                    v.Y = p.Speed.Y + 0.15;
+                    p.Speed = v;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
