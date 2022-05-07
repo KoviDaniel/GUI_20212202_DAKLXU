f81e3b19d9ed18dd43e69ef8d49ea565f3835b29
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-05-07 10:19:02 +0200

    creating a buff enum

--- WpfApp1/Models/Chest.cs
+++ WpfApp1/Models/Chest.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ShoresOfGold.Models
 {
+    enum Buffs 
+    {
+        HealthBuff, MeleeDamageBuff, RangeDamageBuff, SpeedBuff
+    }
     public class Chest
     {
+        
     }
 }
